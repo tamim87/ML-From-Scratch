547d63852d69c93057b75e8d1d60fd256a36de72
tamim <noormuhammadtamim@gmail.com>
2022-05-18 01:13:17 +0600

    add java

---
+++ student.java
@@ -0,0 +1,22 @@
+public class Student{
+    private String name;
+
+    Student()
+    {
+
+    }
+
+    Student(String _name)
+    {
+        this.name = _name;
+    }
+    
+
+    public String getname(){
+        return name;
+    }
+
+    public void setname(String _name){
+        this.name = _name;
+    }
+}
